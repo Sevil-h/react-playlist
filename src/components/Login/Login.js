54b1172ec37cbce98e3da1317ebff6d35631f303
Sevil Hatipoglu <sevilhatipogluu93@gmail.com>
2022-05-08 21:17:53 +0300

    i router-dom

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,22 +3,11 @@ import React from "react";
 const clientId = "97ae56778f9c4caab0fc2382acbf8c75";
 const redirectUri = "http://localhost:3000";
 const clientSecret = "57d21fe0371e446da955d2327edc1eb7";
-
-app.get("/login", function (req, res) {
-	var state = generateRandomString(16);
-	var scope = "user-read-private user-read-email";
-
-	res.redirect(
-		"https://accounts.spotify.com/authorize?" +
-			querystring.stringify({
-				response_type: "code",
-				client_id: clientId,
-				scope: scope,
-				redirect_uri: redirectUri,
-				state: state,
-			})
-	);
-});
+const scopes = ["user-library-read", "playlist-read-private"];
+const authEndpoint = "https://accounts.spotify.com/authorize?";
+const loginEndpoint = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+	"%20"
+)}&response_type=token&show_dialog=true`;
 
 const Login = () => {
 	return (
@@ -26,7 +15,9 @@ const Login = () => {
 			className="container d-flex justify-content-center align-items-center"
 			style={{ minHeight: "100vh" }}
 		>
-			<button className="btn btn-primary">Login</button>
+			<a href={loginEndpoint} className="btn btn-primary btn-lg">
+				Login with Spotify
+			</a>
 		</div>
 	);
 };
---
+++ src/context/useAuth.js
@@ -0,0 +1,3 @@
+import React, { useState, useEffect, useContext, createContext } from "react";
+
+const AuthContext = createContext();
