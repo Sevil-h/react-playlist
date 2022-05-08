ef01dd98c7928838111388cab48e157c019cbcaa
Sevil Hatipoglu <sevilhatipogluu93@gmail.com>
2022-05-08 20:48:15 +0300

    i axios

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,30 @@
 import React from "react";
 
+const clientId = "97ae56778f9c4caab0fc2382acbf8c75";
+const redirectUri = "http://localhost:3000";
+const clientSecret = "57d21fe0371e446da955d2327edc1eb7";
+
+app.get("/login", function (req, res) {
+	var state = generateRandomString(16);
+	var scope = "user-read-private user-read-email";
+
+	res.redirect(
+		"https://accounts.spotify.com/authorize?" +
+			querystring.stringify({
+				response_type: "code",
+				client_id: clientId,
+				scope: scope,
+				redirect_uri: redirectUri,
+				state: state,
+			})
+	);
+});
+
 const Login = () => {
 	return (
 		<div
 			className="container d-flex justify-content-center align-items-center"
-			style={{ height: "100vh" }}
+			style={{ minHeight: "100vh" }}
 		>
 			<button className="btn btn-primary">Login</button>
 		</div>
