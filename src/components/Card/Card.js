0954e5a7e40171ab08d084916ab7f20456e5c7b7
Sevil Hatipoglu <sevilhatipogluu93@gmail.com>
2022-05-11 01:53:22 +0300

    added playlist

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,33 +1,123 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Card.module.css";
 import { FaPlayCircle, FaRegStopCircle } from "react-icons/fa";
 import { BsFillSuitHeartFill, BsArrowRepeat } from "react-icons/bs";
-import Lines from "./Lines";
+import Playlist from "./Playlist";
+import "./index.css";
 
 const Card = () => {
+	const [isPlaying, setIsPlaying] = useState(true);
+	const [isShow, setIsShow] = useState(false);
+
+	// let animation;
+	// let index = 0;
+	// const lines = document.querySelectorAll(".line");
+	// if (isPlaying) {
+	// 	animation = setInterval(() => {
+	// 		lines[index].classList.remove("color");
+	// 		index += 1;
+	// 	}, 1000);
+	// } else {
+	// 	clearInterval(animation);
+	// }
+
+	const contextChangeHandler = () => {
+		setIsShow(!isShow);
+	};
+
 	return (
 		<div className={classes.card}>
-			<div className={classes.playContainer}>
-				<img
-					src="https://i.pinimg.com/564x/3d/a7/67/3da767e496b4b28276f852ee7c81d702.jpg"
-					className={classes.image}
-					alt="image"
-				/>
-				<FaPlayCircle className={classes.icon} />
-				{/* <FaRegStopCircle className={classes.icon} /> */}
-				<div className={classes.smallEffect}></div>
-				<div className={classes.bigEffect}></div>
+			<div className={classes.playlist}>
+				<a href="#" className={classes.link} onClick={contextChangeHandler}>
+					Playlist
+				</a>
 			</div>
-			<div className={classes.details}>
-				<h2>Tearing me up</h2>
-				<h4>Bob Mmoses</h4>
-				{/* <Lines /> */}
-				<p className={classes.time}>4.05</p>
-			</div>
-			<div className={classes.icons}>
-				<BsArrowRepeat className={classes.socialIcon} />
-				<BsFillSuitHeartFill className={classes.socialIcon} />
-			</div>
+			{!isShow && (
+				<div className={classes.play}>
+					<div className={classes.playContainer}>
+						<img
+							src="https://i.pinimg.com/564x/3d/a7/67/3da767e496b4b28276f852ee7c81d702.jpg"
+							className={isPlaying ? classes.image : classes.stop}
+							alt="image"
+						/>
+						{isPlaying ? (
+							<FaPlayCircle
+								className={classes.icon}
+								onClick={() => setIsPlaying(false)}
+							/>
+						) : (
+							<FaRegStopCircle
+								className={classes.icon}
+								onClick={() => setIsPlaying(true)}
+							/>
+						)}
+						<div className={classes.smallEffect}></div>
+						<div className={classes.bigEffect}></div>
+					</div>
+					<div className={classes.details}>
+						<h2>Tearing me up</h2>
+						<h4>Bob Mmoses</h4>
+						<div className="linesWrapper">
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+							<div className="line color"></div>
+						</div>
+						<p className={classes.time}>4.05</p>
+					</div>
+					<div className={classes.icons}>
+						<BsArrowRepeat className={classes.socialIcon} />
+						<BsFillSuitHeartFill className={classes.socialIcon} />
+					</div>
+				</div>
+			)}
+			{isShow && <Playlist />}
 		</div>
 	);
 };
